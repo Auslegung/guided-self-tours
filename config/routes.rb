3a2c003afd375b3422b8d19b421ace14d53abc5b
Jason Fry <jmphry@gmail.com>
2016-11-11 16:25:42 -0500

    Add all routes that correspond to methods in Users Controller
    User routes are create, show, update, index, destroy, add_marker, update_marker, and remove_marker

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+
+  resources :users, only: [:create, :show, :update, :index, :destroy] do
+    collection do
+      post '/login', to: 'users#login'
+    end # end collection do
+
+    member do
+      post '/add_marker', to: 'users#add_marker'
+      patch '/update_marker/:marker_id', to: 'users#update_marker'
+      delete 'remove_marker/:marker_id', to: 'users#remove_marker'
+    end # end member do
+  end # end resources do
+
+  
+end # end draw do

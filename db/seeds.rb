fc797659d76a157416209776a0409398bd6136a3
Jason Fry <jmphry@gmail.com>
2016-11-29 18:10:25 -0500

    Add faker data to the database for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  User.create(username: Faker::Beer.malts, password: Faker::Beer.malts)
+end
+
+10.times do |i|
+  Marker.create(title: Faker::Book.title, description: Faker::Hipster.sentence, audio: Faker::Internet.url, category: Faker::Commerce.department(1), user_id: i)
+end

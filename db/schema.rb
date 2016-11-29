fb96e6dddd7ae5331c5b0ef4217fcabc7bda88cc
Jason Fry <jmphry@gmail.com>
2016-11-29 18:15:40 -0500

    Create markers table in the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111220100) do
+ActiveRecord::Schema.define(version: 20161129223719) do
+
+  create_table "markers", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "audio"
+    t.string   "category"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_markers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
